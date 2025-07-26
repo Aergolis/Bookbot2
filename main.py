d1e47ed6bbec1f25d06cd7e94bae6275148ca5bc
Aergolis <adamufilip01@gmail.com>
2025-07-26 06:27:17 +0200

    oprava předchozího commitu

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from stats import get_world_count
+from stats import get_char_count
+
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
       book_text = f.read()
     return book_text
 
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print(book_text)
 
+    total_words = get_world_count(book_text)
+    print(f"{total_words} words found in the document")
+
+    char_count = get_char_count(book_text)
+    print(char_count)
+    
+   
 main()
